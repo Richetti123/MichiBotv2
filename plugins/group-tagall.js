3b29cf8841f9ab5ac3798c7ce4b1ff90366f7392
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-10-28 03:39:40 +0100

    Change admin permission to false for tagall command

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -22,6 +22,6 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
 handler.tags = ['group'];
 handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
-handler.admin = true;
+handler.admin = false;
 handler.group = true;
 export default handler;
