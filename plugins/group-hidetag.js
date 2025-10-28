aa35a356806dcfba340eba164f29ddc1256ff53e
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-10-28 03:39:39 +0100

    Update group-hidetag.js

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -85,5 +85,5 @@ handler.help = ['hidetag'];
 handler.tags = ['group'];
 handler.command = /^(hidetag|notificar|notify|tag|viso|aviso)$/i;
 handler.group = true;
-handler.admin = true;
+handler.admin = false;
 export default handler;
